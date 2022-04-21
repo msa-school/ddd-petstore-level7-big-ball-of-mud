41d1dfd38feb6fc78de81fdc96e1b659ad01f659
Jinyoung Jang <jyjang@uengine.org>
2022-04-21 05:35:27 +0000

    add

--- src/main/java/com/demo/petstore/domain/CatRepository.java
+++ src/main/java/com/demo/petstore/domain/CatRepository.java
@@ -3,7 +3,7 @@ package com.demo.petstore.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-// http :8080/cats
+// http :8080/cats   <--- CRUD 에 대한 REST API 자동생성 
 public interface CatRepository extends JpaRepository<Cat, Integer>{
     
 }
